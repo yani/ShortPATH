ef452ee248137245694506dc7f1baf28f0726316
Yani <yani.bussens@gmail.com>
2018-07-25 23:45:56 +0200

    init

---
+++ ShortPATH/Form1.cs
@@ -0,0 +1,490 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ShortPATH
+{
+    public partial class Form1 : Form
+    {
+        private Boolean FirstRun = false;
+        private Boolean PATHUpdated = false;
+
+        private string AppTitle = "ShortPATH";
+        private string DefaultNewShortcutIdentifier = "new_shortcut";
+        private List<string> CommandFileExtensions = new List<string>() { "exe", "bat", "cmd", "msi" };
+
+        private string DirectoryPath;
+
+        private List<Shortcut> shortcuts;
+
+        const int HWND_BROADCAST = 0xffff;
+        const uint WM_SETTINGCHANGE = 0x001a;
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern bool SendNotifyMessage(IntPtr hWnd, uint Msg,
+            UIntPtr wParam, string lParam);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            DirectoryPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\" + AppTitle;
+
+            // See if we can access (or create & access) the application directory in AppData
+            if (!CheckAppData())
+            {
+                MessageBox.Show("Failed to load application data directory.", AppTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                System.Environment.Exit(0);
+            }
+
+            // See if the application directory is in the PATH environment variable (or add & check)
+            if (!CheckPATH())
+            {
+                MessageBox.Show("Failed add shortcut directory to PATH environment variable.", AppTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                System.Environment.Exit(0);
+            }
+
+            // See if we can load any Shortcuts
+            if (!LoadShortcuts() || !LoadShortcutList())
+            {
+                MessageBox.Show("Failed to load shortcuts.", AppTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                System.Environment.Exit(0);
+            }
+            
+            Console.WriteLine(AppTitle + " loaded");
+        }
+
+        // Check if we can access (or create & access) the application directory in AppData
+        public Boolean CheckAppData()
+        {
+            try
+            {
+                // If the application directory exists we're done here
+                if (Directory.Exists(DirectoryPath))
+                {
+                    return true;
+                }
+
+                // Create the directory
+                DirectoryInfo di = Directory.CreateDirectory(DirectoryPath);
+
+                // No exception means the directory has been created
+                Console.WriteLine("Directory created: " + DirectoryPath);
+
+                // This mostly indicates a firstrun so we can show an informative message later
+                FirstRun = true;
+
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("The process failed: {0}", exception.ToString());
+            }
+            
+            return false;
+        }
+
+        // Check if the application directory is in the PATH environment variable (or add & check)
+        public Boolean CheckPATH(Boolean addToPATH = true)
+        {
+            // Store into CURRENT_USER for easy portability
+            string registeryKey = "HKEY_CURRENT_USER\\Environment";
+            string registeryValue = "Path";
+            
+            try
+            {
+
+                // Get current paths from registery
+                string pathVariable = (string) Registry.GetValue(registeryKey, registeryValue, "");
+                string[] paths = pathVariable.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                // A list to remember all current directory paths in the current PATH variable if we must edit it
+                List<string> pathList = new List<string>();
+
+                // See if our directory is present
+                foreach (string path in paths)
+                {
+                    pathList.Add(path);
+
+                    if (path == DirectoryPath) {
+                        return true;
+                    }
+                }
+
+                // addToPATH will be false if this is our second try to add the path
+                if(addToPATH == false) {
+                    return false;
+                }
+
+                // Add our directory to the PATH variable list
+                pathList.Add(DirectoryPath);
+
+                // Set the new registry value
+                pathVariable = String.Join(";", pathList);
+                Registry.SetValue(registeryKey, registeryValue, pathVariable);
+                Console.WriteLine("PATH updated: " + pathVariable);
+
+                // Signal the environment so the new PATH is used (not accurate)
+                SendNotifyMessage((IntPtr)HWND_BROADCAST, WM_SETTINGCHANGE, (UIntPtr)0, "Environment");
+
+                // Show a nice message to explain things
+                MessageBox.Show(
+                    "The PATH Environment variable of this user account has been updated. " + 
+                    "You will have to sign out and sign in again before changes are reflected. " + 
+                    "After that changes to the shortcuts will update instantly.",
+                    AppTitle,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+
+                // If the PATH was updated we can make the messagebox when a file is saved context-aware
+                PATHUpdated = true;
+
+                // Double check the PATH
+                return CheckPATH(false);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("The process failed: {0}", exception.ToString());
+            }
+
+            return false;
+        }
+
+        // Load all Shortcuts into a List
+        private Boolean LoadShortcuts()
+        {
+            shortcuts = new List<Shortcut>();
+
+            DirectoryInfo dir = new DirectoryInfo(DirectoryPath);
+            FileInfo[] Files = dir.GetFiles("*.bat");
+
+            foreach (FileInfo file in Files)
+            {
+                string name = Path.GetFileNameWithoutExtension(file.Name);
+                shortcuts.Add(new Shortcut(DirectoryPath, name));
+            }
+
+            return true;
+        }
+
+        // Load the Shortcut List into the ListBox
+        public Boolean LoadShortcutList(string selectedItem = null)
+        {
+            try
+            {
+                // Update Data source
+                listBox1.DataSource = null;
+                listBox1.DataSource = shortcuts;
+                listBox1.DisplayMember = "Identifier";
+                listBox1.ValueMember = "Folder";
+
+                // Check if we need to select an item
+                if(selectedItem == null)
+                {
+                    listBox1.SelectedIndex = -1;
+                } else
+                {
+                    // Get index in ListBox by string
+                    int index = listBox1.FindString(selectedItem);
+
+                    // Select it if it's found
+                    if (index != -1)
+                    {
+                        listBox1.SetSelected(index, true);
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception exception) {
+                Console.WriteLine("The process failed: {0}", exception.ToString());
+            }
+
+            return false;
+        }
+
+        // Load the right panel when the listbox updates
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // The index of the listbox matches the index in the list of Shortcuts
+            int index = listBox1.SelectedIndex;
+
+            // If nothing selected reset the right panel
+            if (index == -1)
+            {
+                textBox1.Text = "";
+                textBox1.Enabled = false;
+
+                textBox2.Text = "";
+                textBox2.Enabled = false;
+
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+
+            } else
+            {
+                // Load the shortcut into the right panel
+                try
+                {
+                    Shortcut shortcut = shortcuts.ElementAt(index);
+
+                    textBox1.Text = shortcut.Identifier;
+                    textBox1.Enabled = true;
+
+                    textBox2.Text = shortcut.Folder;
+                    textBox2.Enabled = true;
+
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                    button4.Enabled = true;
+
+                } catch (Exception exception){
+                    Console.WriteLine("The process failed: {0}", exception.ToString());
+                }
+                
+            }
+
+
+        }
+
+        // Save
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Check for filled in fields
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Both the shortcut identifier and folder should be filled in.",
+                    AppTitle, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            // Check for a valid filename
+            if (!IsValidFilename(textBox1.Text))
+            {
+                MessageBox.Show("Invalid shortcut identifier. It should be able to be saved as a file",
+                    AppTitle, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            // Check for a valid directory path
+            if (!IsValidDirectoryPath(textBox2.Text))
+            {
+                MessageBox.Show("Invalid folder path", AppTitle, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            // The index of the listbox matches the index in the list of Shortcuts
+            int index = listBox1.SelectedIndex;
+
+            if (index != -1)
+            {
+                // Get the Shortcut from the list and update its properties, temporary Shortcuts are also stored in the list
+                Shortcut shortcut = shortcuts.ElementAt(index);
+
+                // Make sure the Shortcut does not exist yet by checking the ListBox
+                if(shortcut.Identifier != textBox1.Text)
+                {
+                    if (listBox1.FindString(textBox1.Text) != -1)
+                    {
+                        MessageBox.Show("This shortcut identifier is already in use.", AppTitle, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        return;
+                    }
+                }
+
+                // Check if the command is already in use (second parameter is the ignored Shortcut identifier)
+                if (CommandExistsInPATH(textBox1.Text, shortcut.Identifier))
+                {
+                    MessageBox.Show("The command \"" + textBox1.Text + "\" is already in use by the environment.",
+                        AppTitle, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+
+                // Shortcut object properties
+                shortcut.Identifier = textBox1.Text;
+                shortcut.Folder = textBox2.Text;
+                shortcut.SaveFile();
+
+                // Reload everything
+                LoadShortcuts();
+                LoadShortcutList();
+
+                // On the first run we'll show a messagebox for more information
+                if (FirstRun)
+                {
+                    // When the PATH is updated as well we'll show this context-aware messagebox
+                    if (PATHUpdated)
+                    {
+                        MessageBox.Show(
+                            "When you sign out and back in again, you will be able to run commands directly in the chosen folder by running: " +
+                            "\"" + shortcut.Identifier + " <command>\". ",
+                            AppTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    } else
+                    {
+                        MessageBox.Show("You can now run commands directly in the chosen folder by running: " +
+                            "\"" + shortcut.Identifier + " <command>\". ",
+                            AppTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+
+                    // Next saves shouldn't annoy the user
+                    FirstRun = false;
+                    PATHUpdated = false;
+                }
+            }
+        }
+
+        // Add
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Reload shortcuts to remove any non saved shortcuts
+            LoadShortcuts();
+
+            // Check for the default new shortcut string and get the next available one if it already exists
+            string shortcutIdentifier = DefaultNewShortcutIdentifier;
+            if (listBox1.FindString(shortcutIdentifier) != -1)
+            {
+                int nextNumber = 2;
+
+                // While we've not found the next identifier
+                while (shortcutIdentifier == DefaultNewShortcutIdentifier)
+                {
+                    string nextShortcutIdentifier = DefaultNewShortcutIdentifier + nextNumber.ToString(); 
+
+                    // If the next identifier is not yet in the list we'll leave the while loop by updating the identifier we'll use
+                    if(listBox1.FindString(nextShortcutIdentifier) == -1)
+                    {
+                        shortcutIdentifier = nextShortcutIdentifier;
+                    } else
+                    {
+                        nextNumber++;
+                    }
+                }
+            }
+
+            // Add a shortcut to the list and show it in the right panel. It is not saved yet at this point
+            shortcuts.Add(new Shortcut(DirectoryPath, shortcutIdentifier));
+            LoadShortcutList(shortcutIdentifier);
+
+            // Select the shortcut text
+            textBox1.Select();
+        }
+
+        // Remove
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // The index of the listbox matches the index in the list of Shortcuts
+            int index = listBox1.SelectedIndex;
+
+            if (index != -1)
+            {
+                // Select Shortcut and delete it
+                Shortcut shortcut = shortcuts.ElementAt(index);
+                shortcut.Delete();
+
+                // Reload everything
+                LoadShortcuts();
+                LoadShortcutList();
+            }
+        }
+
+        // Browse folder
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    textBox2.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        // Simulate a Save button click when we press enter in the field
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button3_Click(this, new EventArgs());
+            }
+        }
+
+        // Simulate a Save button click when we press enter in the field
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button3_Click(this, new EventArgs());
+            }
+        }
+
+        // Check if a string is a valid filename
+        bool IsValidFilename(string testName)
+        {
+            // https://stackoverflow.com/a/62855/5865844
+            Regex containsABadCharacter = new Regex("["
+                  + Regex.Escape(new string(System.IO.Path.GetInvalidFileNameChars())) + "]");
+            if (containsABadCharacter.IsMatch(testName)) { return false; };
+
+            return true;
+        }
+
+        // Check if a string is a valid directory path
+        private bool IsValidDirectoryPath(string path, bool exactPath = true)
+        {
+            // https://stackoverflow.com/a/48820213/5865844
+            bool isValid = true;
+
+            try {
+                string fullPath = Path.GetFullPath(path);
+
+                if (exactPath){
+                    string root = Path.GetPathRoot(path);
+                    isValid = string.IsNullOrEmpty(root.Trim(new char[] { '\\', '/' })) == false;
+                } else {
+                    isValid = Path.IsPathRooted(path);
+                }
+            } catch (Exception ex) {
+                isValid = false;
+            }
+
+            return isValid;
+        }
+
+        // Check if the given string can already be used as a command
+        private bool CommandExistsInPATH(string possibleCommand, string ignoredIdentifier = null)
+        {
+            string PATH = System.Environment.GetEnvironmentVariable("PATH");
+            string[] paths = PATH.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // See if our directory is present
+            foreach (string path in paths)
+            {
+                string currentDirectoryPath = path.Trim(new[] { '\\', '/' });
+
+                if(currentDirectoryPath != DirectoryPath)
+                {
+                    // https://stackoverflow.com/a/50856314/5865844
+                    //foreach (var fileExtension in new[] { "exe", "bat", "cmd", "msi" })
+                    foreach (var fileExtension in CommandFileExtensions)
+                    {
+                        string filePath = currentDirectoryPath + "\\" + possibleCommand + "." + fileExtension;
+                        if (File.Exists(filePath))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            
+            return false;
+        }
+    }
+}
---
+++ ShortPATH/Shortcut.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ShortPATH
+{
+    class Shortcut
+    {
+        private string DirectoryPath;
+
+        private string FilePath;
+
+        public string Identifier { get; set; }
+
+        public string Folder { get; set; }
+            
+        private string FileContents = "@echo off\r\n" +
+                                      "SET org_dir=%cd%\r\n" +
+                                      "cd /d \"{folder}\"\r\n" +
+                                      "%*\r\n" +
+                                      "cd /d \"%org_dir%\"";
+
+        public Shortcut(string directoryPath, string identifier = null)
+        {
+            DirectoryPath = directoryPath;
+
+            if(identifier != null)
+            {
+                Identifier = identifier;
+                FilePath = GetFilePath(identifier);
+                LoadFile();
+            }
+        }
+
+        private Boolean LoadFile()
+        {
+            if (File.Exists(FilePath))
+            {
+                string contents = null;
+
+                try
+                {
+                    using (StreamReader streamReader = new StreamReader(FilePath, Encoding.UTF8))
+                    {
+                        contents = streamReader.ReadToEnd();
+                    }
+
+                } catch(Exception exception)
+                {
+                    Console.WriteLine("The process failed: {0}", exception.ToString());
+                }
+
+                if (!String.IsNullOrEmpty(contents))
+                {
+                    Regex regex = new Regex("cd /d \"(.+?)\"");
+                    Match match = regex.Match(contents);
+
+                    if (match.Success)
+                    {
+                        string folder = match.Groups[1].ToString();
+
+                        if (!String.IsNullOrEmpty(folder))
+                        {
+                            Folder = folder.Replace("\\\\", "\\");
+
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public void SaveFile()
+        {
+            string outputFile = GetFilePath(Identifier);
+
+            // If the identifier has changed we should remove the old file
+            if (outputFile != FilePath) {
+                File.Delete(FilePath);
+            }
+
+            string contents = FileContents.Replace("{folder}", Folder.Replace("\\", "\\\\"));
+
+            File.WriteAllText(outputFile, contents);
+
+            FilePath = outputFile;
+        }
+
+        public void Delete()
+        {
+            if (File.Exists(FilePath))
+            {
+                File.Delete(FilePath);
+            }
+        }
+
+        private string GetFilePath(string identifier)
+        {
+            return DirectoryPath + "\\" + identifier + ".bat";
+        }
+
+        public override string ToString() {
+            return Identifier;
+        }
+    }
+}
